733bbd3b9002e4a88e8874e070e7c506df7c0f01
Darshan004 <badhandarshan@gmail.com>
2020-09-24 19:58:36 +0530

    Addded Sign in page and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
 import HomePage from './page/homepage/homepage.component';
+import ShopPage from './page/shop/shop.component';
+import Header from './component/header/header.component';
+import SignInSignUpPage from './page/sign-in-sign-up-page/sign-in-sign-up-page.component';
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component = {ShopPage} />
+        <Route path='/signin' component = {SignInSignUpPage} />
+        
+      </Switch>
     </div>
   );
 }
---
+++ src/page/sign-in-sign-up-page/sign-in-sign-up-page.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sign-in-sign-up-page.styles.scss';
+
+import  SignIn from  '../../component/sign-in/sign-in.component'
+
+const signInSignUpPage = () => (
+    <div className = 'sign-in-sign-up-page'>
+        <SignIn/>
+    </div>
+)
+
+export default signInSignUpPage;
